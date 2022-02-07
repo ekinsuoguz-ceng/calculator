c5265bd8f55ecfece65f7664f7a11133b7369e87
Ekinsu Oğuz <96749621+sadeceekinsu@users.noreply.github.com>
2022-02-07 23:12:39 +0300

    Add files via upload

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+int main () {
+	
+double a,b;
+int secim;
+
+cout<<"Birinci sayiyi giriniz : ";
+cin>>a;
+cout<<"Ikinci sayiyi giriniz : ";
+cin>>b;
+
+while ( a==0 || b==0) {
+  	cout<<"Lutfen 0'dan farkli bi deger giriniz.";
+
+	
+	cout<<"\nBirinci sayiyi giriniz : ";
+cin>>a;
+cout<<"Ikinci sayiyi giriniz : ";
+cin>>b;
+	
+	cout<<"Islem seciniz  ";
+cout<<"\n1-Toplama islemi"<<"\n2-Cikarma islemi"<<"\n3-Carpma islemi"<<"\n4-Bolme islemi"<<"\n5-Cikis yapiniz";
+cout<<"\nSeciminiz : ";
+cin>>secim;
+}
+while(secim<1 && secim>5) {
+	cout<<"Lutfen 1 ve 5 arasýnda bir secim yapiniz"<<"\nSecim";
+	cin>>secim;
+}
+
+cout<<"Ýslem seciniz  ";
+cout<<"\n1-Toplama islemi"<<"\n2-Cikarma islemi"<<"\n3-Carpma islemi"<<"\n4-Bolme islemi"<<"\n5-Cikis yapiniz";
+cout<<"\nSeciminiz : ";
+cin>>secim;
+while(secim<1 && secim>5) {
+	cout<<"Lutfen 1 ve 5 arasýnda bir secim yapiniz"<<"\nSecim";
+	cin>>secim;
+}
+
+if(secim==1){
+	cout<<a<<"+"<<b<<"="<<a+b;
+}
+else if(secim==2) {
+	cout<<a<<"-"<<b<<"="<<a-b;
+}
+else if (secim==3) {
+	cout<<a<<"*"<<b<<"="<<a*b;
+	
+}
+else if(secim==4){
+	cout<<a<<"/"<<b<<"="<<a/b;
+}
+else {
+	cout<<"Cikis yaptiniz.";
+}
+
+return 0;
+
+}
+
